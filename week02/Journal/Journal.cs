1c0215e4b7945d09a2f077f9de675a37342e7c21
hydapig <moh8287@gmail.com>
2025-03-17 18:18:50 -0600

    completed journal program

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+        _entries.Add(entry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (var entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using var ouputFile = new StreamWriter(filename);
+        foreach (var entry in _entries)
+        {
+            ouputFile.WriteLine($"{entry._date}~{entry._promptText}~{entry._entryText}");
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        // _entries.Clear();
+        string[] lines = System.IO.File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("~");
+
+            Entry entry = new Entry()
+            {
+                _date = parts[0],
+                _promptText = parts[1],
+                _entryText = parts[2]
+            };
+            AddEntry(entry);
+        };
+    }
+}
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,71 @@
 using System;
+using System.IO.Enumeration;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Welcom to the Journal Program.");
+
+        bool quit = false;
+        PromptGenorator prompt = new PromptGenorator();
+        
+        Journal journal = new Journal();
+        
+
+        while (!quit)
+        {
+            Console.WriteLine("Please choose one of the following:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display entries");
+            Console.WriteLine("3. Save journal to a file");
+            Console.WriteLine("4. Load journal from a file");
+            Console.WriteLine("5. Exit");
+            Console.Write("What would you like to do? ");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                string randomPrompt = prompt.GetRandomPrompt();
+                Console.Write("");
+                Console.WriteLine(randomPrompt);
+                string text = Console.ReadLine();
+
+                Entry entry = new Entry();
+                entry._date = DateTime.Now.ToString("dd/MM/yyyy");
+                entry._promptText = randomPrompt;
+                entry._entryText = text;
+
+                journal._entries.Add(entry);
+            }
+            else if (input == "2")
+            {
+                Console.WriteLine("");
+                journal.DisplayAll();
+            }
+            else if (input == "3")
+            {
+                Console.WriteLine("Please enter a file name: ");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+                Console.WriteLine("Your journal has been saved.");
+                Console.WriteLine("");
+            }
+            else if (input == "4")
+            {
+                Console.WriteLine("Enter a file name: ");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+                Console.WriteLine("Your journal has been uploaded...");
+            }
+            else if (input == "5")
+            {
+                Console.WriteLine("Thank you for writing in your journal today!");
+                quit = true;
+            }
+            else{
+                Console.WriteLine("Please enter a valid choice");
+            }
+        }
     }
 }
