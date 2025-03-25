62aa36e7aef303adc840f156c715744660aa148d
hydapig <moh8287@gmail.com>
2025-03-25 11:09:47 -0600

    added conditional to verify user input is an integer

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -6,16 +6,17 @@ class Program
     {
         Reference reference = new Reference("Doctrine and Covenants", 121, 7, 8);
         Scripture scripture = new Scripture(reference, "My son, peace be unto thy soul; thine adversity and thine afflictions shall be but a small moment; And then, if thou endure it well, God shall exalt thee on high; thou shalt triumph over all thy foes.");
-        
+
         //prompt the user to choose how many words to hide
         string input;
+        Console.WriteLine("Welcome to the Scripture Memorizer Program!");
         Console.WriteLine("How many words do you want to hide at once? Pick between 1 and 3: ");
         
         //validates whether the user entered the number correctly
         while (true)
         {
             input = Console.ReadLine();
-            if (int.Parse(input) >= 1 && int.Parse(input) <= 3)
+            if (int.TryParse(input, out _) && int.Parse(input) >= 1 && int.Parse(input) <= 3)
             {
                 break;
             }
