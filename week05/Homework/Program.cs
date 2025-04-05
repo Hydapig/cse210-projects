652c01a708af5cd534fa4165e95a333f1c308656
hydapig <moh8287@gmail.com>
2025-04-05 13:29:16 -0600

    completed inheritance practice

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment math1 = new MathAssignment("Andrew","Multiplication","3.4","1-12");
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment writing1 = new WritingAssignment("Blake","American History","The Causes of the American Civil War");
+        Console.WriteLine(writing1.GetWritingInformation());
+
     }
 }
