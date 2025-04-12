41fc0cfd575f4f80976f9944a003e28b9a48034a
hydapig <moh8287@gmail.com>
2025-04-11 20:54:45 -0600

    completed shapes assignment

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square s1 = new Square(12,"blue");
+        Rectangle r1 = new Rectangle(7,14,"red");
+        Circle c1 = new Circle(47,"white");
+
+        List<Shape> shapes = new List<Shape>{s1,r1,c1};
+        
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape's area is {area}");
+        }
+
     }
 }
